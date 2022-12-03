b2c1d4090b0d7da4f45497d9b3c1d0de0c143fc5
Brian <brianderidder2007@gmail.com>
2022-12-03 23:25:20 +0000

    Reset

--- app.js
+++
@@ -1,9 +0,0 @@
-function changeText(elem, defaultText) {
-    console.log(elem, defaultText)
-
-    if (elem.innerHTML == defaultText) {
-        elem.innerHTML = 'Home';
-    } else {
-        elem.innerHTML = defaultText;
-    }
-}
